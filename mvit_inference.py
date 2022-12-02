4b4fba9f419756525098261dee590b1e9797a255
Caelen <caelenmensch@gmail.com>
2022-12-01 16:16:05 -0800

    mvit inference

---
+++ mvit_inference.py
@@ -0,0 +1,23 @@
+from torchvision.io.video import read_video
+from torchvision.models.video.mvit import mvit_v2_s, MViT_V2_S_Weights
+
+vid, _, _ = read_video("dataset/vatex_test_videos/_0ZBlXUcaOk_000013_000023.mp4", output_format="TCHW")
+#vid = vid[:32]  # optionally shorten duration
+
+# Step 1: Initialize model with the best available weights
+weights = MViT_V2_S_Weights.DEFAULT
+model = mvit_v2_s(weights=weights)
+model.eval()
+
+# Step 2: Initialize the inference transforms
+preprocess = weights.transforms()
+
+# Step 3: Apply inference preprocessing transforms
+batch = preprocess(vid).unsqueeze(0)
+
+# Step 4: Use the model and print the predicted category
+prediction = model(batch).squeeze(0).softmax(0)
+label = prediction.argmax().item()
+score = prediction[label].item()
+category_name = weights.meta["categories"][label]
+print(f"{category_name}: {100 * score}%")
