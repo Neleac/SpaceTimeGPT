bc61e62af5b836da97705f2381b7ab597bd4212c
Caelen <caelenmensch@gmail.com>
2022-12-05 09:57:53 -0800

    try swin

--- mvit_inference.py
+++ mvit_inference.py
@@ -1,12 +1,15 @@
 from torchvision.io.video import read_video
-from torchvision.models.video.mvit import mvit_v2_s, MViT_V2_S_Weights
+#from torchvision.models.video.mvit import mvit_v2_s, MViT_V2_S_Weights
+from torchvision.models.video.swin_transformer import swin3d_t, Swin3D_T_Weights
 
 vid, _, _ = read_video("dataset/vatex_test_videos/_0ZBlXUcaOk_000013_000023.mp4", output_format="TCHW")
 #vid = vid[:32]  # optionally shorten duration
 
 # Step 1: Initialize model with the best available weights
-weights = MViT_V2_S_Weights.DEFAULT
-model = mvit_v2_s(weights=weights)
+# weights = MViT_V2_S_Weights.DEFAULT
+# model = mvit_v2_s(weights=weights)
+weights = Swin3D_T_Weights.DEFAULT
+model = swin3d_t(weights=weights)
 model.eval()
 
 # Step 2: Initialize the inference transforms
