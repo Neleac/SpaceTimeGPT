5fd9cdf75ba1b1ebb580e7fa58439b9483df6fd8
922201615 <922201615@srv-hss-157-53.at.sfsu.edu>
2023-04-02 02:00:33 -0700

    random frames training, all captions eval

---
+++ train.py
@@ -0,0 +1,111 @@
+import json
+import os
+import random
+
+from datasets import load_from_disk
+import evaluate
+import numpy as np
+import torch
+from transformers import VisionEncoderDecoderModel, AutoImageProcessor, AutoTokenizer, Seq2SeqTrainer, Seq2SeqTrainingArguments, EarlyStoppingCallback
+
+device = "cuda"
+torch.backends.cuda.matmul.allow_tf32 = True
+
+encoder = "facebook/timesformer-base-finetuned-k600"
+decoder = "gpt2"
+
+image_processor = AutoImageProcessor.from_pretrained("MCG-NJU/videomae-base")
+tokenizer = AutoTokenizer.from_pretrained(decoder)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(encoder, decoder).to(device)
+model.config.decoder_start_token_id = tokenizer.bos_token_id
+model.config.pad_token_id = tokenizer.pad_token_id
+model.config.max_length = 50
+model.config.num_beams = 4
+model.config.early_stopping = True
+
+dataset = load_from_disk("dataset/processed/k600_16frames")
+dataset.set_format("torch")
+
+output_dir="training/random_frames"
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir=output_dir,
+    tf32=True,
+    predict_with_generate=True,
+    load_best_model_at_end=True,
+    logging_strategy="epoch",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    per_device_train_batch_size=6,
+    per_device_eval_batch_size=6,
+    dataloader_num_workers=8,
+    num_train_epochs=100,
+    learning_rate=5e-7,
+)
+
+def collator(examples):
+    pixel_values, labels = [], []
+    for example in examples:
+        # train
+        if len(example["pixel_values"]) == 16:
+            frame_idxs = []
+            for i in range(0, 16, 2):
+                frame_idxs.append(i + random.randint(0, 1))
+            pixel_values.append(example["pixel_values"][frame_idxs])
+        # val
+        else:
+            pixel_values.append(example["pixel_values"])
+        labels.append(example["labels"])
+
+    pixel_values = torch.stack(pixel_values)
+    labels = torch.stack(labels)
+    return {"pixel_values": pixel_values, "labels": labels}
+
+bleu = evaluate.load("bleu")
+meteor = evaluate.load("meteor")
+rouge = evaluate.load("rouge")
+val_output = {}
+with open("dataset/longestCaption_videoID.json") as file:
+    longestCaption_videoID = json.load(file)
+with open("dataset/videoID_captions.json") as file:
+    videoID_captions = json.load(file)
+    
+def metrics(eval_predictions):
+    predictions, labels = eval_predictions
+    predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+    references = []
+    for i, label in enumerate(labels):
+        video_id = longestCaption_videoID[label]
+        references.append(videoID_captions[video_id])
+        if video_id in val_output:
+            val_output[video_id].append(predictions[i])
+        else:
+            val_output[video_id] = [predictions[i]]
+            
+    for reference in references:
+        print(reference)
+            
+    bleu_scores = bleu.compute(predictions=predictions, references=references, smooth=True)
+    meteor_scores = meteor.compute(predictions=predictions, references=references)
+    rouge_scores = rouge.compute(predictions=predictions, references=references, rouge_types=['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)    
+    return {"bleu": bleu_scores["bleu"], "meteor": meteor_scores["meteor"], "rouge1": rouge_scores["rouge1"], "rouge2": rouge_scores["rouge2"], "rougeL": rouge_scores["rougeL"]}
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["validation"],
+    data_collator=collator,
+    compute_metrics=metrics,
+    callbacks = [EarlyStoppingCallback(early_stopping_patience=5)],
+)
+
+trainer.train()
+
+with open(os.path.join(output_dir, "val_output.json"), "w") as file:
+    file.write(json.dumps(val_output))
